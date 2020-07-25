164967a0bb0ca4384aaae9a252bfb59ac45556d2
florez18399 <andresfflorez1999@hotmail.com>
2020-07-25 18:53:10 -0500

    Agregado hbs como  motor de plantillas

--- Middleware/serverM.js
+++ Middleware/serverM.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
-
-const PORT = 3011;
+const path = require('path');
+const hbs = require('express-handlebars');
+//-----------------------------
+const PORT = 3010;
+const servers = ['http:localhost:3011', 'http:localhost:3012', 'http:localhost:3013']
+///------------------------------------
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', hbs({
+    defaultLayout: 'main', 
+    layoutsDir : path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+///----------------------------------
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
-app.listen(3000, function (){
+app.listen(PORT, function (){
   console.log('Middleware en puerto ', PORT)
 })
