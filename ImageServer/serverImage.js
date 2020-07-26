4e9d2c6493b3df3a92c1015a612ff3d748931873
florez18399 <andresfflorez1999@hotmail.com>
2020-07-25 21:52:58 -0500

    Metodo de calculo de espacio en la carpeta de imagenes

--- ImageServer/serverImage.js
+++ ImageServer/serverImage.js
@@ -1,20 +1,35 @@
 var express = require('express');
 var app = express();
-
+const fs = require('fs');
+const path = require('path')
 const PORT = process.argv[2];
-
+const gfs = require('get-folder-size');
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.get('/saveImage', (req, res) => {
-    res.send('Espacio utilizado...')
+    res.send('EN construccion' )
+})
+
+app.get('/spaceUsage', (req, res) => {
+    checkUsageSpace((size) => {
+        res.json({size: size});
+    });
 })
 
 app.listen(PORT, function (){
   console.log('Servidor de imagenes en puerto ', PORT)
 })
 
-function checkUsageSpace() {
-    
+function checkUsageSpace(cb) {
+    gfs(path.join(__dirname, '/public/uploads'), (err, size) => {
+        if(err) {
+            console.log('Error al obtener espacio utilizado');
+            return null;
+        }else {
+            console.log(size);
+            cb((size / 1024 / 1024).toFixed(2));
+        }
+    })
 }
